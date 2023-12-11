38afe735098a541c8acad5e1c577c00470ee0b97
ManishReddyK <manish.koppula@iiitb.ac.in>
2023-12-11 18:18:12 +0530

    res

--- backend/src/main/java/com/courier/delivery/controllers/AuthController.java
+++ backend/src/main/java/com/courier/delivery/controllers/AuthController.java
@@ -20,11 +20,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 import java.util.Optional;
 
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
 @RestController
 @CrossOrigin
